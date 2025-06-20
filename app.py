bb61f53c31f13881c9ba00a1d4a86278f70cd932
Towhid Ahmed <121598055+TowhidAhmedd@users.noreply.github.com>
2025-06-20 21:14:47 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pickle 
+import pandas as pd
+
+movies_dict = pickle.load(open('movies_dict.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
+st.title('Movie Recommend System')
+
+option = st.selectbox('How would you like to be contacted?',
+            movies['title'].values)
+
